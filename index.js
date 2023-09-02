d526b3eaa19b4c460e2fb9f463d44f2bddbb9a86
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-03 02:27:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
         var myTicket;
-        var baseURL = 'http://localhost/otcs/cs.exe';
         
-        // var baseURL = document.getElementById("baseURL").value;
-        var userName = document.getElementById("userName").value;
-        var password = document.getElementById("password").value;
-        var credentials = { userName: userName, password: password };
+        
+        var baseURL;
+        var userName;
+        var password;
+        
+        var credentials;
         // var url = baseURL + '/api/v1/auth';
         // Function to authenticate the user
         function authenticateUser() {
+        baseURL = document.getElementById("baseURL").value;
+        userName = document.getElementById("userName").value;
+        password = document.getElementById("password").value;
+        credentials = { userName: userName, password: password };
             var url = baseURL + '/api/v1/auth';
 
             $.support.cors = true;
