2e7d1f9fc0d7e3578adea0d11262d6771045ab37
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-02 21:38:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+        var myTicket;
+        // var baseURL = 'http://localhost/otcs/cs.exe';
+        
+        var baseURL = document.getElementById("baseURL").value;
+        var userName = document.getElementById("userName").value;
+        var password = document.getElementById("password").value;
+        var credentials = { userName: userName, password: password };
+        // Function to authenticate the user
+        function authenticateUser() {
+            var url = baseURL + '/api/v1/auth';
+
+            $.support.cors = true;
+            $.ajax({
+                url: url,
+                type: "POST",
+                crossDomain: true,
+                data: credentials,
+                dataType: "json",
+                contentType: "application/x-www-form-urlencoded",
+                success: function (res) {
+                    document.getElementById("messageDiv").innerHTML = "OTCSTicket For Your Reference==> " +res.ticket;
+                    alert("OTCSTicket is Generated");
+                    myTicket = res.ticket;
+                },
+                error: function (res) {
+                    document.getElementById("messageDiv").innerHTML = "Something Error happened! " + res.statusText;
+                    alert("Something Error happened! " + res.statusText);
+                }
+            });
+        }
