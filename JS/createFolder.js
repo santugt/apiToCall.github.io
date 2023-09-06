5d0216160b0d9119cd36479488af8467937fd2cb
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-06 17:29:01 +0530

    Update createFolder.js

--- JS/createFolder.js
+++ JS/createFolder.js
@@ -1,1 +1,29 @@
+function fCreateFolder() {
+alert(myTicket);
 
+
+
+  var myBody = { type: "0", parent_id: "2000", name: "Project - "};
+
+
+$(document).ready(function(){
+
+var url = baseURL+'/api/v1/nodes';
+$.support.cors = true;
+$.ajax({
+url:url,
+type:"POST",
+crossDomain: true,
+data:myBody,
+dataType:"json",
+headers: { "OTCSTICKET": myTicket },
+success:function(res){
+alert("success!");
+alert("Folder object id = " + res.id);
+},
+error:function(res){
+alert("Bad thing happened! " + res.statusText);
+}
+});
+});
+}
