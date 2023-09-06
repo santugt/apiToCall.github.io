7a84548b3a9b23f5e4ca2f5cc3d5b5656a21d67c
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-06 19:25:36 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -89,7 +89,7 @@ function updateFolderNameDescription() {
     };
 
     // Assuming you have 'baseURL' and 'myTicket' defined somewhere in your code
-    var url = baseURL + '/v1/nodes/' + nodeId;
+    var url = 'https://cors-anywhere.herokuapp.com/'+baseURL + '/v1/nodes/' + nodeId;
 
     $.support.cors = true;
     $.ajax({
