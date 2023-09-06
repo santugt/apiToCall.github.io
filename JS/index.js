f6305694d37f97c061456570a3cd5e966a5b79a3
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-06 17:24:58 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -38,35 +38,35 @@
 
         }
 
-function fCreateFolder() {
-alert(myTicket);
+// function fCreateFolder() {
+// alert(myTicket);
 
 
 
-  var myBody = { type: "0", parent_id: "2000", name: "Project - "};
+//   var myBody = { type: "0", parent_id: "2000", name: "Project - "};
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-var url = baseURL+'/api/v1/nodes';
-$.support.cors = true;
-$.ajax({
-url:url,
-type:"POST",
-crossDomain: true,
-data:myBody,
-dataType:"json",
-headers: { "OTCSTICKET": myTicket },
-success:function(res){
-alert("success!");
-alert("Folder object id = " + res.id);
-},
-error:function(res){
-alert("Bad thing happened! " + res.statusText);
-}
-});
-});
-}
+// var url = baseURL+'/api/v1/nodes';
+// $.support.cors = true;
+// $.ajax({
+// url:url,
+// type:"POST",
+// crossDomain: true,
+// data:myBody,
+// dataType:"json",
+// headers: { "OTCSTICKET": myTicket },
+// success:function(res){
+// alert("success!");
+// alert("Folder object id = " + res.id);
+// },
+// error:function(res){
+// alert("Bad thing happened! " + res.statusText);
+// }
+// });
+// });
+// }
 
 
 
