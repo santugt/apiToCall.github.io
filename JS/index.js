21f908ca88de74cd055771c35ddab0892db3dc13
Santosh Tiparashi <94696565+santugt@users.noreply.github.com>
2023-09-06 18:31:28 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -76,6 +76,44 @@ alert("Bad thing happened! " + res.statusText);
 }
 
 
+function CreateFolder() {
+alert(myTicket);
+
+
+
+  
+            var parentID = document.getElementById("parentID").value;
+            var folderName = document.getElementById("folderName").value;
+
+            // Create the request body
+            var myBody = {
+                type: '0',
+                parent_id: parentID,
+                name: folderName
+            };
+$(document).ready(function(){
+
+var url = baseURL+'/api/v1/nodes';
+$.support.cors = true;
+$.ajax({
+url:url,
+type:"POST",
+crossDomain: true,
+data:myBody,
+dataType:"json",
+headers: { "OTCSTICKET": myTicket },
+success:function(res){
+alert("success!");
+alert("Folder object id = " + res.id);
+},
+error:function(res){
+alert("Bad thing happened! " + res.statusText);
+}
+});
+});
+}
+
+
 
 
 
